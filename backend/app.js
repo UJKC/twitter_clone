1cb2bea17ae50e19615360bea5cf4e2a1ab45f3c
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-13 13:51:50 +0530

    css fault

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const { readdirSync } = require('fs');
 const middleware = require('./helper/middleware/middleware')
-const path = require('path');
 const app = express();
 
-const backendPath = path.resolve(__dirname);
 
-app.use(express.static(path.join(backendPath, '../frontend/src/css')));
 readdirSync('backend/route').map((r) => app.use('/', middleware.requireLogin, require('./route/' + r)));
 
 const server = app.listen(3000, () => {
