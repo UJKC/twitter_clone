2277aabb271e65624652eb8cf89be2b01d985a1b
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-13 12:23:45 +0530

    login page success

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const { readdirSync } = require('fs');
 const middleware = require('./helper/middleware/middleware')
 
-readdirSync('backend/route').map((r) => app.use('/', r !== 'login' ? middleware.requireLogin : [], require('./route/' + r)));
+readdirSync('backend/route').map((r) => app.use('/', middleware.requireLogin, require('./route/' + r)));
 
 const server = app.listen(3000, () => {
     console.log("Server is on on 3000 port. Dont mind everything is fine.");
