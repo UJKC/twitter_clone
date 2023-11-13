50b8193297f4cea881e42d5700b926303396ee4b
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-13 13:37:09 +0530

    css not rendered

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
 const { readdirSync } = require('fs');
 const middleware = require('./helper/middleware/middleware')
+const path = require('path');
+const app = express();
 
+const backendPath = path.resolve(__dirname);
+
+app.use(express.static(path.join(backendPath, '../frontend/src/css')));
 readdirSync('backend/route').map((r) => app.use('/', middleware.requireLogin, require('./route/' + r)));
 
 const server = app.listen(3000, () => {
