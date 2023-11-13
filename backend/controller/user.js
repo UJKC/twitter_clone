c96819678f1e723456718a33d62dbadc23dbec08
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-13 17:27:27 +0530

    empty field working

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -21,25 +21,29 @@ exports.register = (req, res, next) => {
 
 exports.register_post = (req, res, next) => {
     var payload = req.body;
-    var firstName = req.body.firstName.trim();
-    var lastName = req.body.lastName.trim();
-    var username = req.body.username.trim();
-    var password = req.body.password.trim();
-    var email = req.body.email.trim();
-    var gender = req.body.gender.trim();
-    var bday = req.body.bday.trim();
-    var bmonth = req.body.bmonth.trim();
-    var byear = req.body.byear.trim();
+
+    // Check if the properties exist before using trim
+    var firstName = payload.firstName ? payload.firstName.trim() : '';
+    var lastName = payload.lastName ? payload.lastName.trim() : '';
+    var username = payload.username ? payload.username.trim() : '';
+    var password = payload.password ? payload.password.trim() : '';
+    var email = payload.email ? payload.email.trim() : '';
+    var gender = payload.gender ? payload.gender.trim() : '';
+    var bday = payload.bday ? payload.bday.trim() : '';
+    var bmonth = payload.bmonth ? payload.bmonth.trim() : '';
+    var byear = payload.byear ? payload.byear.trim() : '';
+
     if (firstName && lastName && username && password && email && gender && bday && bmonth && byear) {
         console.log("Noted");
-        res.send(req.body)
-    }
-    else {
-        res.status(200).render('/register', payload);
+        res.json(payload);
+    } else {
+        // Adjust your rendering logic here
+        res.status(200).render('register');
     }
 }
 /*
 CHeck for password is same two times
 Cryptic done
 Check for empty fields
+Get values back
 */
