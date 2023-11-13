0c1478ed122450bd4575923ae6381b8fcc3916f5
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-13 11:05:37 +0530

    middleware explaination

--- backend/helper/middleware/middleware.js
+++ backend/helper/middleware/middleware.js
@@ -5,3 +5,43 @@ exports.requireLogin = (req, res, next) => {
         return res.redirect('/login');
     }
 };
+
+/*Certainly! Let's break down each line of the provided middleware:
+
+```javascript
+exports.requireLogin = (req, res, next) => {
+```
+
+1. `exports.requireLogin`: This line exports the middleware function as `requireLogin`. It allows other parts of your code to use this middleware by importing it with `require('./path/to/middleware').requireLogin`.
+
+2. `(req, res, next) => {`: This part declares an arrow function, which is the actual middleware function. The function takes three parameters:
+
+   - `req`: The request object represents the HTTP request and contains information about the request, such as headers, parameters, and the request body.
+   - `res`: The response object represents the HTTP response that the middleware can modify before sending it back to the client.
+   - `next`: A callback function that, when called, passes control to the next middleware in the stack. If `next` is not called, the request remains within the current middleware.
+
+```javascript
+    if (req.session && req.session.user) {
+```
+
+3. `if (req.session && req.session.user)`: This line checks if there is a `session` object in the request and if that session object has a `user` property. It's assuming that user information is stored in the session.
+
+   - `req.session`: Represents the session object associated with the request. It is typically used to store user-specific information.
+   - `req.session.user`: The assumption here is that the user is considered logged in if there is a `user` property in the session.
+
+```javascript
+        return next();
+    } else {
+```
+
+4. `return next();`: If the condition in the previous line is true (meaning there is a valid user session), this line calls the `next` function, allowing the request to proceed to the next middleware or route handler in the stack.
+
+```javascript
+        return res.redirect('/login');
+    }
+```
+
+5. `return res.redirect('/login');`: If the condition in the `if` statement is false (meaning there is no valid user session), this line sends a redirect response to the client, instructing the browser to navigate to the `/login` URL.
+
+This middleware, therefore, checks if a user is logged in based on the presence of a user object in the session. If the user is logged in, it allows the request to proceed; otherwise, it redirects the user to the login page.
+*/
