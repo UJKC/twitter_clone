858764fe37cf02602dd6ad503a8c5914c3e61959
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-15 23:40:54 +0530

    working name

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -21,21 +21,40 @@ exports.register = (req, res, next) => {
 }
 
 exports.register_post = async (req, res, next) => {
-    var payload = req.body;
-    const name = payload.name;
-    check = await User.findOne({name: name})
-    if (check) {
-        const user = User.create(payload)
-    }
-    else {
-        res.status(400).json({
-            message: "User already exist"
-        })
-    }
+    const payload = req.body;
+const { username, email } = payload;
+
+// Check if the username is already in use
+const existingUsername = await User.findOne({ username });
+if (existingUsername) {
+    return res.status(400).json({
+        message: "Username is already in use",
+        username
+    });
+}
+
+// Check if the email is already in use
+const existingEmail = await User.findOne({ email });
+if (existingEmail) {
+    return res.status(400).json({
+        message: "Email is already in use",
+        email
+    });
+}
+
+// If neither the username nor email is in use, create a new user
+const user = await User.create(payload);
+console.log("User created successfully");
+
+// Return a success response or perform any additional actions
+return res.status(200).json({
+    message: "User created successfully",
+    user
+});
+
 }
 /*
 CHeck for password is same two times
-Cryptic done
 Check for empty fields
 Get values back
 */
